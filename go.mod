54ca3dd1efb068c6d69e1e61f9b15e4ec4d22731
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-04-29 23:14:18 +0300

    main()

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/mux v1.7.1
 	github.com/studtool/common v0.0.0-20190429195109-599ea62b8c20
+	go.uber.org/dig v1.7.1-0.20190327003609-4cc9f8898fe3
 )
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
 
+	"go.uber.org/dig"
+
+	"github.com/studtool/common/utils"
+
+	"github.com/studtool/documents-service/api"
+	"github.com/studtool/documents-service/beans"
+)
+
+func main() {
+	c := dig.New()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Kill)
+	signal.Notify(ch, os.Interrupt)
+
+	utils.AssertOk(c.Provide(api.NewServer))
+	utils.AssertOk(c.Invoke(func(srv *api.Server) {
+		go func() {
+			if err := srv.Run(); err != nil {
+				beans.Logger().Fatal(err)
+				ch <- os.Interrupt
+			}
+		}()
+	}))
+	defer func() {
+		utils.AssertOk(c.Invoke(func(srv *api.Server) {
+			if err := srv.Shutdown(); err != nil {
+				beans.Logger().Fatal(err)
+			}
+		}))
+	}()
+
+	<-ch
 }
