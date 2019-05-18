96183d459c21e5edc404b63d0440772068e4c69a
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-18 23:40:42 +0300

    dependencies update

--- config/config.go
+++ config/config.go
@@ -7,27 +7,27 @@ import (
 )
 
 var (
-	_ = func() *config.FlagVar {
-		f := config.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_LOG_ENV_VARS", false)
+	_ = func() *cconfig.FlagVar {
+		f := cconfig.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_LOG_ENV_VARS", false)
 		if f.Value() {
-			config.SetLogger(beans.Logger())
+			cconfig.SetLogger(beans.Logger())
 		}
 		return f
 	}()
 
-	ServerPort = config.NewStringDefault("STUDTOOL_DOCUMENTS_SERVICE_PORT", "80")
+	ServerPort = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_SERVICE_PORT", "80")
 
-	CorsAllowed         = config.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_ALLOW_CORS", false)
-	RequestsLogsEnabled = config.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_LOG_REQUESTS", true)
+	CorsAllowed         = cconfig.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_ALLOW_CORS", false)
+	RequestsLogsEnabled = cconfig.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_SHOULD_LOG_REQUESTS", true)
 
-	RepositoriesEnabled = config.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_REPOSITORIES_ENABLED", false)
+	RepositoriesEnabled = cconfig.NewFlagDefault("STUDTOOL_DOCUMENTS_SERVICE_REPOSITORIES_ENABLED", false)
 
-	DocsDir = config.NewStringDefault("STUDTOOL_DOCUMENTS_DIRECTORY_PATH", "./documents")
+	DocsDir = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_DIRECTORY_PATH", "./documents")
 
-	StorageHost     = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_HOST", "127.0.0.1")
-	StoragePort     = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_PORT", "5432")
-	StorageDB       = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_NAME", "documents")
-	StorageUser     = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_USER", "user")
-	StoragePassword = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_PASSWORD", "password")
-	StorageSSL      = config.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_SSL_MODE", "disable")
+	StorageHost     = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_HOST", "127.0.0.1")
+	StoragePort     = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_PORT", "5432")
+	StorageDB       = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_NAME", "documents")
+	StorageUser     = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_USER", "user")
+	StoragePassword = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_PASSWORD", "password")
+	StorageSSL      = cconfig.NewStringDefault("STUDTOOL_DOCUMENTS_INFO_STORAGE_SSL_MODE", "disable")
 )
--- go.mod
+++ go.mod
@@ -7,10 +7,9 @@ go 1.12
 require (
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/handlers v1.4.0
-	github.com/gorilla/mux v1.7.1
+	github.com/gorilla/mux v1.7.2
 	github.com/lib/pq v1.1.1
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
-	github.com/studtool/common v0.0.0-20190507195319-d8fbd7bce61b
+	github.com/studtool/common v0.0.0-20190518195043-e1b27f5c60c5
 	go.uber.org/dig v1.7.1-0.20190327003609-4cc9f8898fe3
-	golang.org/x/sys v0.0.0-20190508100423-12bbe5a7a520 // indirect
 )
