ef5f27693a07bc008aebde14034f504a226b3366
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-28 23:35:19 +0300

    documents content service implementation

--- logic/impl/documentsContentService.go
+++ logic/impl/documentsContentService.go
@@ -7,6 +7,7 @@ import (
 	"github.com/studtool/common/logs"
 
 	"github.com/studtool/documents-service/logic"
+	"github.com/studtool/documents-service/models"
 	"github.com/studtool/documents-service/repositories"
 	"github.com/studtool/documents-service/utils"
 )
@@ -14,6 +15,9 @@ import (
 type DocumentsContentService struct {
 	documentsInfoRepository    repositories.DocumentsInfoRepository
 	documentsContentRepository repositories.DocumentsContentRepository
+
+	documentNotFoundErr     *errs.Error
+	documentAccessDeniedErr *errs.Error
 
 	structLogger  logs.Logger
 	reflectLogger logs.Logger
@@ -30,6 +34,8 @@ func NewDocumentsContentService(params DocumentsContentServiceParams) *Documents
 	r := &DocumentsContentService{
 		documentsInfoRepository:    params.DocumentsInfoRepository,
 		documentsContentRepository: params.DocumentsContentRepository,
+		documentNotFoundErr:        errs.NewNotFoundError("document not found"),
+		documentAccessDeniedErr:    errs.NewPermissionDeniedError("document access denied"),
 	}
 
 	r.structLogger = srvutils.MakeStructLogger(r)
@@ -41,9 +47,39 @@ func NewDocumentsContentService(params DocumentsContentServiceParams) *Documents
 }
 
 func (s *DocumentsContentService) GetDocumentContent(params *logic.GetDocumentContentParams) *errs.Error {
-	panic("implement me")
+	var documentInfo models.DocumentInfo
+	if err := s.documentsInfoRepository.GetDocumentInfoByID(&documentInfo); err != nil {
+		s.structLogger.Warningf("document [id = '%s'] not found", params.DocumentID)
+		return s.documentNotFoundErr
+	}
+
+	if params.UserID != documentInfo.OwnerID {
+		s.structLogger.Warningf(
+			"document [id = '%s'] access forbidden [user_id = '%s'; scope = 'read']",
+			params.DocumentID, params.UserID,
+		)
+		return s.documentAccessDeniedErr
+	}
+
+	return s.documentsContentRepository.
+		GetDocumentContent(params.DocumentID, params.DocumentContent)
 }
 
 func (s *DocumentsContentService) UpdateDocumentContent(params *logic.UpdateDocumentContentParams) *errs.Error {
-	panic("implement me")
+	var documentInfo models.DocumentInfo
+	if err := s.documentsInfoRepository.GetDocumentInfoByID(&documentInfo); err != nil {
+		s.structLogger.Warningf("document [id = '%s'] not found", params.DocumentID)
+		return s.documentNotFoundErr
+	}
+
+	if params.UserID != documentInfo.OwnerID {
+		s.structLogger.Warningf(
+			"document [id = '%s'] access forbidden [user_id = '%s'; scope = 'write']",
+			params.DocumentID, params.UserID,
+		)
+		return s.documentAccessDeniedErr
+	}
+
+	return s.documentsContentRepository.
+		UpdateDocumentContent(params.DocumentID, params.DocumentContent)
 }
