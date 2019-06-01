64e7d3c1ab44e6a0086db2a3392aa46ac385d3ee
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-06-01 23:41:02 +0300

    dependencies update

--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/gorilla/mux v1.7.2
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
 	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
-	github.com/studtool/common v0.0.0-20190601183610-5777bc1d1eb5
+	github.com/studtool/common v0.0.0-20190601204030-b85a2b83cbce
 	github.com/tinylib/msgp v1.1.0
 	go.uber.org/dig v1.7.1-0.20190327003609-4cc9f8898fe3
 	google.golang.org/appengine v1.6.0 // indirect
--- vendor/github.com/studtool/common/logs/logsExporter.go
+++ vendor/github.com/studtool/common/logs/logsExporter.go
@@ -1,11 +1,18 @@
 package logs
 
 import (
+	"fmt"
+	"net"
+
+	"github.com/firstrow/goautosocket"
+
 	"github.com/studtool/logrustash"
 )
 
 type Exporter struct {
-	params     ExporterParams
+	params ExporterParams
+
+	conn       net.Conn
 	logrusHook *logrustash.Hook
 }
 
@@ -22,19 +29,28 @@ func NewLogsExporter(params ExporterParams) *Exporter {
 }
 
 func (e *Exporter) OpenConnection() error {
-	hook, err := logrustash.NewHook(
-		"tcp", e.params.StorageAddress,
-		e.params.ComponentName+":"+e.params.ComponentVersion,
-	)
+	conn, err := gas.Dial("tcp", e.params.StorageAddress)
 	if err != nil {
 		return err
 	}
+
+	appName := fmt.Sprintf("%s:%s",
+		e.params.ComponentName, e.params.ComponentVersion,
+	)
+
+	hook, err := logrustash.NewHookWithConn(conn, appName)
+	if err != nil {
+		return err
+	}
+
+	e.conn = conn
 	e.logrusHook = hook
+
 	return nil
 }
 
 func (e *Exporter) CloseConnection() error {
-	return nil //TODO
+	return e.conn.Close()
 }
 
 func (e *Exporter) getHook() *logrustash.Hook {
